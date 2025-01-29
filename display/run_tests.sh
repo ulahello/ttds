6e788db788bc87a9d94ab0099423c65e5162f595
Ula Shipman <ula.hello@mailbox.org>
2025-01-28 19:39:29 -0800

    feat(run_tests.sh): display PASS/FAIL summary at the end
    Signed-off-by: Ula Shipman <ula.hello@mailbox.org>

--- display/run_tests.sh
+++ display/run_tests.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-set -e
 
 PREFIX='expect-'
 dump_dir='./tests'
@@ -18,15 +17,32 @@ elif [ "$#" -eq 1 ]; then
 	dump_dir="$1"
 fi
 
-find "$dump_dir" -type f -name "${PREFIX}*.data" | while IFS= read -r expect_path
-do
-	test_name=$(basename "$expect_path")
-	test_name=${test_name#"$PREFIX"}
-	test_dir=$(dirname "$expect_path")
-	test_path="${test_dir}/${test_name}"
+find "$dump_dir" -type f -name "${PREFIX}*.data" |
+{
+	count_pass=0
+	count_fail=0
 
-	diff "$expect_path" "$test_path"
-	echo "$test_path OK"
-done
+	while IFS= read -r expect_path
+	do
+		test_name=$(basename "$expect_path")
+		test_name=${test_name#"$PREFIX"}
+		test_dir=$(dirname "$expect_path")
+		test_path="${test_dir}/${test_name}"
 
-echo 'All tests OK'
+		if diff "$expect_path" "$test_path"; then
+			echo "$0: $test_path PASSED"
+			count_pass=$((count_pass + 1))
+		else
+			echo "$0: $test_path FAILED"
+			count_fail=$((count_fail + 1))
+		fi
+	done
+	count_total=$((count_pass + count_fail))
+
+	echo "$count_pass/$count_total tests PASSED"
+	if [ $count_fail -eq 0 ]; then
+		exit 0
+	else
+		exit 1
+	fi
+}
