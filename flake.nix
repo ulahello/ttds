b35c1af88543d4b94c737a507446ee0779f0cda5
Tali Auster <me@tali.network>
2025-02-18 20:16:27 -0800

    feat: initialize a flake

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux; in
+      pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+        buildInputs = with pkgs; [ systemd.dev meson ninja libdrm pkg-config ];
+
+        CC = "clang";
+      };
+  };
+}
