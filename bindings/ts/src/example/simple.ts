90f1163cb167e5d1810a04615243b79443c7c82c
leodog896 <leodog896@gmail.com>
2025-03-15 12:51:51 -0700

    feat: delete pane

--- bindings/ts/src/example/simple.ts
+++ bindings/ts/src/example/simple.ts
@@ -3,3 +3,7 @@ import { Pane } from "../mod";
 const pane = await Pane.init({ color: "#ffffff", baseURL: "https://example.com" });
 
 pane.draw({ type: 'rectangle', w: 500, h: 500, x: 20, y: 40, color: "#dddddd" })
+
+setTimeout(async () => {
+    await pane.delete()
+}, 5000)
