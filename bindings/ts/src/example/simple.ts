199027a72ba14968dde49b3e09bbc0389417b75c
leodog896 <leodog896@gmail.com>
2025-03-15 13:54:44 -0700

    feat: tsconfig

--- bindings/ts/src/example/simple.ts
+++ bindings/ts/src/example/simple.ts
@@ -1,9 +1,17 @@
 import { Pane } from "../mod";
 
-const pane = await Pane.init({ color: "#ffffff", baseURL: "https://example.com" });
+function sleep(ms: number) {
+    return new Promise((res) => setTimeout(res, ms))
+}
 
-pane.draw({ type: 'rectangle', w: 500, h: 500, x: 20, y: 40, color: "#dddddd" })
+for (let i = 0; i < 10; i++) {
+    const pane = await Pane.init({ color: "#ffffff", baseURL: "https://ttds.tali.network" });
 
-setTimeout(async () => {
-    await pane.delete()
-}, 5000)
+    pane.draw({ type: 'rect', w: 500, h: 500, x: 20, y: 40, color: "#dddddd" })
+
+    setTimeout(async () => {
+        await pane.delete()
+    }, 5000)
+
+    await sleep(1200)
+}
